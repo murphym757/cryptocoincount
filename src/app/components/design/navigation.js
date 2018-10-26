bfa1834cc0ee62bf4aed85eea5bd06f75ae7927f
murphym757 <murphym757@yahoo.com>
2018-10-26 00:03:57 -0400

    Added larger icons for the homepage

--- src/app/components/design/navigation.js
+++ src/app/components/design/navigation.js
@@ -7,8 +7,43 @@ import Logo from '../design/logo.js';
 export default class Navigation extends Component {
     render() {
         return (
-            <div>
-                <CryptoLinks /> 
+            <div class="container-fluid">
+                <div class="row pt-5">
+                    <div class="col-6 btc-Icon">
+                        <Link to="/litecoin" style={{ textDecoration: 'none' }}>
+                            <i class="cf cf-btc btc-links"></i>
+                        </Link>
+                    </div>
+                    <div class="col-6 dash-Icon">
+                        <Link to="/litecoin" style={{ textDecoration: 'none' }}>
+                            <i class="cf cf-dash"></i>
+                        </Link>
+                    </div>
+                    </div>
+                    <div class="row pt-5">
+                    <div class="col-6 eth-Icon">
+                        <Link to="/litecoin" style={{ textDecoration: 'none' }}>
+                            <i class="cf cf-eth"></i>
+                        </Link>
+                    </div>
+                    <div class="col-6 ltc-Icon">
+                        <Link to="/litecoin" style={{ textDecoration: 'none' }}>
+                            <div class="ltc-Link"><i class="cf cf-ltc"></i></div>
+                        </Link>
+                    </div>
+                    </div>
+                    <div class="row pt-5">
+                    <div class="col-6 xmr-Icon">
+                        <Link to="/litecoin" style={{ textDecoration: 'none' }}>
+                            <i class="cf cf-xmr"></i>
+                        </Link>
+                    </div>
+                    <div class="col-6 xrp-Icon">
+                        <Link to="/litecoin" style={{ textDecoration: 'none' }}>
+                            <i class="cf cf-xrp"></i>
+                        </Link>
+                    </div>
+                    </div>
             </div> 
           );
       }
