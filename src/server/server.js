d31534fdd1346b9c58a95e05a039445de8bd5d47
Maurice Murphy <murphym757@yahoo.com>
2022-09-21 12:18:25 -0400

    made a change to bodyParser for railaway

--- src/server/server.js
+++ src/server/server.js
@@ -18,7 +18,7 @@ const express = require('express'),
 ));
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 /* Mongoose 
